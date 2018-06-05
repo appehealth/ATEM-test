e720693a1f87fe32bc510a47c0970986666b5510
appehealth <karl.seiler@stud.leuphana.de>
2018-06-05 16:56:28 +0200

    -

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,39 +5,37 @@ angular.module('App.controllers', [])
         }])
 
     .controller('Comp1Ctrl', ['$scope', '$http', function ($scope, $http) {
-             var allQuestions = [];
-             var iter = 0;
-             var numberOfQuestions = 0;
-             var results = [];
-             $scope.resultsBool = false;
-              $http
-                .get("json/comp1.json")
-                .then(function(response){
-                  allQuestions = response.data.questions;
-                  numberOfQuestions = response.data.numberOfQuestions;
-                  $scope.currentQuestion = allQuestions[iter];
-                });
+        var allQuestions = [];
+        var iter = 0;
+        var numberOfQuestions = 0;
+        var results = [];
+        $scope.resultsBool = false;
+        $http.get("json/comp1.json").then(function(response){
+            allQuestions = response.data.questions;
+            numberOfQuestions = response.data.numberOfQuestions;
+            $scope.currentQuestion = allQuestions[iter];
+        });
 
-              $scope.answerClick = function(i){
-                results[iter] = "Frage " + $scope.currentQuestion.id + ":  Korrekte Antwort: " + $scope.currentQuestion.correctAnswer + "  Gewählte Antwort: " + i;
-                if(iter < numberOfQuestions - 1){
-                    iter = iter + 1;
-                    $scope.currentQuestion = allQuestions[iter];
-                }
-                else{
-                 $scope.resultsBool = true;
-                 }
-              };
+        $scope.answerClick = function(i){
+            results[iter] = "Frage " + $scope.currentQuestion.id + ":  Korrekte Antwort: " +
+                            $scope.currentQuestion.correctAnswer + "  Gewählte Antwort: " + i;
+            if(iter < numberOfQuestions - 1){
+                iter = iter + 1;
+                $scope.currentQuestion = allQuestions[iter];
+            }
+            else{
+                $scope.resultsBool = true;
+            }
+        };
 
-              $scope.showResults = function(){
-                var message = results[0];
-                for(j = 1; j < numberOfQuestions; j++){
-                    message += "\r\n" + results[j];
-                }
-                alert(message);
-              }
-
-        }])
+        $scope.showResults = function(){
+        var message = results[0];
+        for(j = 1; j < numberOfQuestions; j++){
+            message += "\r\n" + results[j];
+        }
+        alert(message);
+        }
+    }])
 
     ;
 
