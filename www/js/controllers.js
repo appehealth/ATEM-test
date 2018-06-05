4532a72153a99020c477f0965cd27d4afc127b6d
appehealth <karl.seiler@stud.leuphana.de>
2018-06-05 11:36:54 +0200

    komponente 1 view neue struktur

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,12 +5,24 @@ angular.module('App.controllers', [])
         }])
 
     .controller('Comp1Ctrl', ['$scope', '$http', function ($scope, $http) {
+             var allQuestions = [];
+             var iter = 0;
              $scope.questions = [];
               $http
                 .get("json/comp1.json")
                 .then(function(response){
                   $scope.questions = response.data.questions;
+                  allQuestions = response.data.questions;
+                  $scope.currentQuestion = allQuestions[iter];
                 });
+
+              $scope.naechsteFrage = function(){
+                if(iter < 1){
+                    iter = iter + 1;
+                    $scope.currentQuestion = allQuestions[iter];
+                }
+                else alert("Letzte Frage erreicht");
+              };
 
         }])
 
--- www/js/services.js
+++ www/js/services.js
@@ -33,4 +33,6 @@ angular.module('App.services', [])
             }
         };
     });
+
+
     ;
