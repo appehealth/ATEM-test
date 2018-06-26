77bef1336ccf15d5e53b4342bfa2851d8b1b5f74
appehealth <39769697+appehealth@users.noreply.github.com>
2018-06-26 12:12:42 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -20,30 +20,7 @@ var app = {
 
         console.log("device ready");
         app.receivedEvent('deviceready');
-        app.createLog();
-
-        }
-
-        createLog: function(){
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(getFileSystem){
-        	console.log('file system open: ' + fs.name);
-        	app.createFile(fs.root, "TestLogFile.csv", false);
-        }
-        }
-
-        createFile: function (dirEntry, fileName, isAppend) {
-            // Creates a new file or returns the file if it already exists.
-            dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
-
-                writeFile(fileEntry, null, isAppend);
-
-            }, onErrorCreateFile);
-
-        }
-
-
-
-
+        },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -54,5 +31,5 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+}
 };
