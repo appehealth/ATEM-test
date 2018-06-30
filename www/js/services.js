841aa6e6c6c208956f545051b1a2fc5222d06324
appehealth <karl.seiler@stud.leuphana.de>
2018-06-30 13:27:24 +0200

    results

--- www/js/services.js
+++ www/js/services.js
@@ -1,46 +1,45 @@
-angular.module('App.services', [])
-    .factory('cordovaReady', [function () {
-        return function (fn) {
-            var queue = [],
-                impl = function () {
-                    queue.push([].slice.call(arguments));
-                };
-
-            document.addEventListener('deviceready', function () {
-                queue.forEach(function (args) {
-                    fn.apply(this, args);
-                });
-                impl = fn;
-            }, false);
-
-            return function () {
-                return impl.apply(this, arguments);
-            };
+angular.module( 'App.services', [] )
+  .factory( 'cordovaReady', [ function() {
+    return function( fn ) {
+      var queue = [],
+        impl = function() {
+          queue.push( [].slice.call( arguments ) );
         };
-    }])
-    .service('storeEvents', function () {
-        var templates = [];
-        var startTime;
 
-       function logEvent (logText, component, item) {
-                var timestamp = Date.now() - startTime;
-                templates.push(timestamp + ': Component ' + component +', Item ' + item + ': ' + logText);
-                console.log(templates);
+      document.addEventListener( 'deviceready', function() {
+        queue.forEach( function( args ) {
+          fn.apply( this, args );
+        } );
+        impl = fn;
+      }, false );
 
-                }
-
-      function logStart(){
-        startTime = Date.now();
-        logEvent('Start');
-      }
-
-      return {
-        logStart: logStart,
-        templates:templates,
-        logEvent: logEvent
-
+      return function() {
+        return impl.apply( this, arguments );
       };
+    };
+  } ] )
+  .service( 'storeEvents', function() {
+    var eventStorage = [];
+    var startTime;
 
+    function logEvent( logText, component, item ) {
+      var timestamp = Date.now() - startTime;
+      eventStorage.push( timestamp + ': Component ' + component + ', Item ' + item + ': ' + logText );
+    }
 
-     })
-    ;
+    function showEvents() {
+      console.log( eventStorage.join( '\n' ) );
+    }
+
+    function logStart() {
+      startTime = Date.now();
+      logEvent( 'Start' );
+    }
+
+    return {
+      logStart: logStart,
+      logEvent: logEvent,
+      showEvents: showEvents
+    };
+
+  } );
